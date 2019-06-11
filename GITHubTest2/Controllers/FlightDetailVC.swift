6c0938ffd1ef465ada34fef3807076cad61ad41f
apple1 <apple1@macmini2.local>
2019-06-11 12:45:15 +0530

    FlightDetail Added

---
+++ GITHubTest2/Controllers/FlightDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  FlightDetailVC.swift
+//  GITHubTest2
+//
+//  Created by apple1 on 11/06/19.
+//  Copyright © 2019 APPLE. All rights reserved.
+//
+
+import UIKit
+
+class FlightDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
